7b85223b029d3ca545b4909becf47ef5fb26850c
karenminasyan123 <karen.minasyan@picsart.com>
2020-10-03 13:12:34 +0400

    Initial Commit

---
+++ ModernCollectionViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernCollectionViewSample
+//
+//  Created by Karen Minasyan on 10/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
